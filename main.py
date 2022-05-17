6d5c3f26b0fdc962cb561d921ff467e95c743a07
Miroslav Savel <miroslav.savel@gmail.com>
2022-05-17 12:43:18 +0200

    both rectangles moving

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, sys
 
 def bouncing_rect():
-    global x_speed,y_speed
+    global x_speed,y_speed,other_speed
     moving_rect.x += x_speed
     moving_rect.y += y_speed
 
@@ -11,18 +11,24 @@ def bouncing_rect():
     if moving_rect.bottom >= screen_height or moving_rect.top <=0:
         y_speed *= -1
 
+    # moving the other rectangle
+    other_rect.y += other_speed
+    if other_rect.top <= 0 or other_rect.bottom >= screen_height:
+        other_speed *= -1
+
+
     # collision between rect, it doesnt tell me where exactly
     # https://inventwithpython.com/invent4thed/chapter19.html
     collision_tolerance = 10
     if moving_rect.colliderect(other_rect):
         print("collision has occured")
-        if abs(other_rect.top - moving_rect.bottom) < collision_tolerance:    # if this is True we know the collision happened on the top
+        if abs(other_rect.top - moving_rect.bottom) < collision_tolerance and y_speed > 0:    # if this is True we know the collision happened on the top
             y_speed *= -1
-        if abs(other_rect.bottom - moving_rect.top) < collision_tolerance:
+        if abs(other_rect.bottom - moving_rect.top) < collision_tolerance and y_speed < 0:
             y_speed *= -1
-        if abs(other_rect.right - moving_rect.left) < collision_tolerance:
+        if abs(other_rect.right - moving_rect.left) < collision_tolerance and x_speed < 0:
             x_speed *= -1
-        if abs(other_rect.left - moving_rect.right) < collision_tolerance:
+        if abs(other_rect.left - moving_rect.right) < collision_tolerance and x_speed > 0:
             x_speed *= -1
     pygame.draw.rect(screen, (255,255,255), moving_rect)
     pygame.draw.rect(screen, (255, 0, 0), other_rect)
